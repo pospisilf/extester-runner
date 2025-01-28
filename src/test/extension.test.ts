a78420652be18f4987b292de6311082ec3253b7c
Filip Pospisil <fpospisi@redhat.com>
2025-01-28 23:52:08 +0100

    settings works

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,17 @@
-import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+describe.only('Main describe only', function () {
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	describe.skip('Nested describe skip', () => {
+		it.only('individual test only ', async function () {
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		});
+		it.skip('individual test skip', async function () {
+
+		});
+		it('individual test', async function () {
+
+		});
+	});
+	describe('Main describe', function () {
 	});
 });
