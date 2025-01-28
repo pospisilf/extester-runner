d87c20e6a0d6518c2a80379a9406fb56ac6bb899
Filip Pospisil <fpospisi@redhat.com>
2025-01-28 22:09:52 +0100

    another small step

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	const treeDataProvider = new EmptyTreeViewProvider();
     vscode.window.registerTreeDataProvider('extesterView', treeDataProvider);
 
-    // Optional: Add a command to refresh the TreeView
-    context.subscriptions.push(
-        vscode.commands.registerCommand('emptyTreeView.refresh', () => {
-            treeDataProvider.refresh();
-        })
-    );
+    
 
-	  // run all tests
-	  context.subscriptions.push(
+	// commands
+	// run all tests
+	context.subscriptions.push(
 		vscode.commands.registerCommand('extester-runner.runAll', async () => {
-		  vscode.window.showInformationMessage('Running all tests...');
+			vscode.window.showInformationMessage('extester-runner.runAll');
 		})
-	  );
+	);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extester-runner" is now active!');
+	// refresh tests
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extester-runner.refreshTests', async () => {
+			vscode.window.showInformationMessage('extester-runner.refreshTests');
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extester-runner.helloWorld', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		await runAllTests();
-		vscode.window.showInformationMessage('Hello World from extester-runner and Jordan Haj!');
-	});
+	// collapse all
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extester-runner.collapseAll', async () => {
+			vscode.window.showInformationMessage('extester-runner.collapseAll');
+		})
+	);
 
-	context.subscriptions.push(disposable);
+	// search files
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extester-runner.searchFiles', async () => {
+			vscode.window.showInformationMessage('extester-runner.searchFiles');
+		})
+	);
+	
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
 
-function runAllTests() {
-	vscode.window.showInformationMessage('runAllTests()');
-}
 
-// Define a basic TreeDataProvider
+
+
+
+
+
+
+
+//// To be replaced!
+
+// Define a basic TreeDataProvider -> To be replaced!`
 class EmptyTreeViewProvider implements vscode.TreeDataProvider<TreeItem> {
     private _onDidChangeTreeData: vscode.EventEmitter<TreeItem | undefined | null | void> =
         new vscode.EventEmitter<TreeItem | undefined | null | void>();
