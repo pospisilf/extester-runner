3ae3b5e37881a83e02f37d75106ba72b6253422e
Filip Pospisil <fpospisi@redhat.com>
2025-02-05 13:41:25 +0100

    read additional params from settings

--- src/tasks/RunFolderTask.ts
+++ src/tasks/RunFolderTask.ts
@@ -4,9 +4,10 @@ import { TestRunner } from './TestRunnerTask';
 export class RunFolderTask extends TestRunner {
     constructor(folder: string) {
         const configuration = workspace.getConfiguration('extesterRunner');
+        const additionalArgs: string[] = configuration.get<string[]>('additionalArgs', []);
         const outputFolder = configuration.get<string>('outFolder') || 'out';
         const outputPath = folder.replace(/src\//, `${outputFolder}/`) + '/**/*.test.js';
-        const shellExecution = new ShellExecution(`npx extest setup-and-run ${outputPath}`);
+        const shellExecution = new ShellExecution(`npx extest setup-and-run ${outputPath} ${additionalArgs.join(' ')}`);
         super(TaskScope.Workspace, 'Run Test Folder', shellExecution);
     }
 }
